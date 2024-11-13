3451441a050a048a3b92158bf70b34f4ee563f93
Anshita Bhasin <anshita.bhasin@gmail.com>
2024-11-13 14:33:08 +0400

    Update basic-click.cy.js

--- cypress/e2e/cypress_click_actions/basic-click.cy.js
+++ cypress/e2e/cypress_click_actions/basic-click.cy.js
@@ -1,19 +1,12 @@
-// describe("Click action testing ", () => {
-//     it("Testing login flow", () => {
-//         cy.visit("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
-//         cy.get('[id="input-email"]').type("lambdatest.Cypress@disposable.com");
-//         cy.get('[id="input-password"]').type("Cypress123!!");
-//         cy.get('[type="submit"]').eq(0).click();
-//     });
-// });
+
 describe('Basic Click Operations', () => {
     beforeEach(() => {
         cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=common/home')
     })
 
     it('performs a basic click on a product category', () => {
-        cy.get('img[alt="Laptops"]').click()
-        cy.url().should('include', 'category')
+        cy.get('img[alt="HTC Touch HD"]').first().click()
+        cy.url().should('include', 'product')
     })
 
     it('clicks on a product and verifies navigation', () => {
